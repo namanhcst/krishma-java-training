5ab691a79cffc4ba693bb9e27efcbd742e452f04
naman.hcst <naman.hcst@gmail.com>
2024-12-01 22:48:20 +0530

    initial

---
+++ src/com/learning/java/P2.java
@@ -0,0 +1,39 @@
+package com.learning.java;
+
+import test.Utility;
+
+import static test.Utility.printS;
+
+ class Practice1 extends Utility {
+	public static void main(String[] args)
+	{	
+			int k=Practice1.sum(10, 25);	 
+		 System.out.println(k);
+		 
+		 Utility u  = new Utility();
+		 Practice1 p1= new Practice1();
+		 u.print();
+		 p1.print();
+		 printS();
+		 
+		 
+		 
+	}
+
+
+	public static int sum(int i, int j){			
+			int k=i+j;
+			return k;
+	}
+}
+
+public class P2{
+	
+	
+	
+}
+
+class P3{
+	
+	
+}
---
+++ src/test/Utility.java
@@ -0,0 +1,34 @@
+package test;
+
+public class Utility {
+
+	protected void print() {
+
+		System.out.println("i am in Test - Utility");
+		
+		common();
+
+	}
+
+	public static void printS() {
+
+	}
+	
+	 static void common() {
+		
+		
+	}
+	
+	
+	
+
+}
+
+
+class NewClass {
+	
+	
+	
+	
+}
+
